6423523e7434e1aba84cb55545dc190d32b899a6
hugozalm <hugozalm@gmail.com>
2025-10-19 18:34:25 +0200

    change nterface to class

--- main.ts
+++ main.ts
@@ -13,14 +13,18 @@ enum TropicalFruit {
  */
 //% weight=70 icon="\uf84c" color=#EC7505
 namespace location {
-    export interface Location {
+    export class Location {
         x: number;
         y: number;
+        constructor(x?: number, y?: number) {
+            this.x = x | -1;
+            this.y = y | -1;
+        }
     }
 
     export class position {
         // currentLocation: {x:number;y:number} = { x: 0, y: 0 };
-        currentLocation: Location = { x: 0, y: 0 };
+        currentLocation: Location = new Location();
         /**
          * Reset system
          */
@@ -29,7 +33,7 @@ namespace location {
         //% weight=85 blockGap=8
         //% parts="location"
         reset(): boolean {
-            this.currentLocation = { x: 0, y: 0 };
+            this.currentLocation = new Location();
             return true;
         }
 
@@ -53,7 +57,7 @@ namespace location {
         //% weight=85 blockGap=8
         //% parts="location"
         goto(x: number, y: number): boolean {
-            this.currentLocation = {x, y };
+            this.currentLocation = new Location(x,y); //{x, y };
             return true;
         }
 
