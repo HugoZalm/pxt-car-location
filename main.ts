3009e6ef530715da2b99715dcd4159faa25cdafe
hugozalm <hugozalm@gmail.com>
2025-10-19 18:05:12 +0200

    added class

--- main.ts
+++ main.ts
@@ -17,31 +17,34 @@ enum TropicalFruit {
  */
 //% weight=70 icon="\uf84c" color=#EC7505
 namespace location {
-    const currentLocation: {x:number;y:number} = { x: 0, y: 0 };
-    /**
-     * Reset system
-     */
-    //% blockId=location_reset block="reset location"
-    export function reset(): boolean {
-        this.currentLocation = { x: 0, y: 0 };
-        return true;
-    }
 
-    /**
-     * Read location
-     */
-    //% blockId=location_read block="read coördinates"
-    export function read(): {x:number;y:number} {
-        return this.currentLocation;
-    }
+    export class location {
+        currentLocation: {x:number;y:number} = { x: 0, y: 0 };
+        /**
+         * Reset system
+         */
+        //% blockId=location_reset block="reset location"
+        reset(): boolean {
+            this.currentLocation = { x: 0, y: 0 };
+            return true;
+        }
 
-    /**
-     * Goto a location
-     */
-    //% blockId=location_goto block="goto %x %y"
-    export function goto(x: number, y: number): boolean {
-        this.currentLocation = {x, y };
-        return true;
-    }
+        /**
+         * Read location
+         */
+        //% blockId=location_read block="read coördinates"
+        read(): {x:number;y:number} {
+            return this.currentLocation;
+        }
 
+        /**
+         * Goto a location
+         */
+        //% blockId=location_goto block="goto %x %y"
+        goto(x: number, y: number): boolean {
+            this.currentLocation = {x, y };
+            return true;
+        }
+
+    }
 }
